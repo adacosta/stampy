c00120b95a0ad076ef03f042ba663c535af56ff1
Alan Da Costa <alandacosta@gmail.com>
2013-04-13 14:10:54 -0700

    Refactor fives and threes strategy to use case/when instead of if/else

--- lib/stampy/strategies/fives_and_threes_strategy.rb
+++ lib/stampy/strategies/fives_and_threes_strategy.rb
@@ -9,17 +9,18 @@ module Stampy
       end
 
       def calculate
-        if fives_remainder == 0
+        case @input % 5
+        when 0
           @fives = @input / 5
-        elsif fives_remainder == 2
+        when 2
           @fives = (@input / 5) - 2
-          @threes = (@input - fives_sum) / 3
-        elsif fives_remainder == 3
+          @threes = (@input - (@fives * 5)) / 3
+        when 3
           @fives = @input / 5
           @threes = 1
         else
           @fives = (@input / 5) - 1
-          @threes = (@input - fives_sum) / 3
+          @threes = (@input - (@fives * 5)) / 3
         end
         {'five' => @fives, 'three' => @threes}
       end
@@ -28,14 +29,6 @@ module Stampy
 
       def validate_input
         raise(ArgumentError, "input must be >= 8") if @input < 8
-      end
-
-      def fives_remainder
-        @input % 5
-      end
-
-      def fives_sum
-        @fives * 5
       end
     end
   end
